c871cdd1ecf8c1fa88c58ff2fdfd28046ba59cf5
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2019-10-22 18:44:20 +0200

    Fix tag

--- src/main/kotlin/com/wtm/dgomez/qa/QaController.kt
+++ src/main/kotlin/com/wtm/dgomez/qa/QaController.kt
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 
-@Controller
+@RestController
 class QaController {
 
     @Autowired

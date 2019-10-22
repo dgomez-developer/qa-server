a6d9c94ba436b8d004c9143dcda35d1a6e97dd73
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2019-10-22 17:57:34 +0200

    Define interface in repository

--- src/main/kotlin/com/wtm/dgomez/qa/QaRepository.kt
+++ src/main/kotlin/com/wtm/dgomez/qa/QaRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-abstract class QaRepository : MongoRepository<QuestionModel, Int>
+interface QaRepository : MongoRepository<QuestionModel, Int>

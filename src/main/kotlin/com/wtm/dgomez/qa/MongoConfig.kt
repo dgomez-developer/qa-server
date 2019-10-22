df1384ac15905b2fce8a17bac70170f836022131
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2019-10-22 17:48:13 +0200

    Add Mongo config

--- src/main/kotlin/com/wtm/dgomez/qa/MongoConfig.kt
+++ src/main/kotlin/com/wtm/dgomez/qa/MongoConfig.kt
@@ -1,9 +1,22 @@
 package com.wtm.dgomez.qa
 
+import com.mongodb.MongoClient
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
 @EnableMongoRepositories
-class MongoConfig {
+@Import(value = MongoAutoConfiguration::class)
+class MongoConfig: AbstractMongoConfiguration() {
+
+    override fun getDatabaseName(): String {
+        return "qa"
+    }
+
+    override fun mongoClient(): MongoClient {
+        return MongoClient("127.0.0.1", 27017)
+    }
 }

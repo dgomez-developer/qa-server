8032f13aeb8a1c852cf9e541801added2cc0e229
Debora Gomez Bertoli <dgomez.developer@gmail.com>
2019-10-22 18:16:30 +0200

    Leave default config

--- src/main/kotlin/com/wtm/dgomez/qa/MongoConfig.kt
+++ src/main/kotlin/com/wtm/dgomez/qa/MongoConfig.kt
@@ -1,22 +1,13 @@
 package com.wtm.dgomez.qa
 
-import com.mongodb.MongoClient
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @Configuration
 @EnableMongoRepositories
 @Import(value = MongoAutoConfiguration::class)
-class MongoConfig: AbstractMongoConfiguration() {
+class MongoConfig{
 
-    override fun getDatabaseName(): String {
-        return "qa"
-    }
-
-    override fun mongoClient(): MongoClient {
-        return MongoClient("127.0.0.1", 27017)
-    }
 }
